ca7ef9559528547bf06090445eecc7fb2a47d975
iwintue <spd.07@mail.ru>
2026-06-02 03:41:48 +0300

    Update tests.py
    были добавлены тесты для класса MailReader

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-mport os
+import os
 import pytest
 
 from mail_classifier import MailClassifier
@@ -115,6 +115,84 @@ class TestMailClassifier:
         }
 
         assert set(classifier.rules.keys()) == expected_folders
+
+class TestMailReader:
+    @pytest.fixture
+    def reader(self):
+        return MailReader()
+    def test_image(self, reader, tmp_path):
+        message = os.path.join(tmp_path, "scooter.jpeg")
+        with open(message, "w", encoding="utf-8") as file:
+            file.write("scooterrr")
+        with pytest.raises(ValueError) as error:
+            reader.read(message)
+        assert str(error.value) == "Изображение"
+    def test_empty(self, reader, tmp_path):
+        message = os.path.join(tmp_path, "vozduh.txt")
+        with open(message, "w", encoding="utf-8") as file:
+            file.write("")
+        with pytest.raises(ValueError) as error:
+            reader.read(str(message))
+        assert str(error.value) == "В файле ничего нет, он пустой"
+    def test_engText(self, reader, tmp_path):
+        message = os.path.join(tmp_path, "engText.txt")
+        text = """From: students.com
+        Subject: происшествие на шаболовской
+        На территории очень много велосипедов"""
+        with open(message, "w", encoding="utf-8") as file:
+            file.write(text)
+        res = reader.read(message)
+        assert res.file_name == "engText.txt"
+        assert res.sender == "students.com"
+        assert res.subject == "происшествие на шаболовской"
+    def test_rusText(self, reader, tmp_path):
+        message = os.path.join(tmp_path, "rusText.txt")
+        text = """От кого: матан.com
+        Тема: скоро экзамен
+        я ненавижу интегралы"""
+        with open(message, "w", encoding="utf-8") as file:
+            file.write(text)
+        res = reader.read(message)
+        assert res.file_name == "rusText.txt"
+        assert res.sender == "матан.com"
+        assert res.subject == "скоро экзамен"
+    def test_translitText(self, reader, tmp_path):
+        message = os.path.join(tmp_path, "translitText.txt")
+        text = """Ot kogo: studentochki.com
+        Tema: pomogite skoro sessia"""
+        with open(message, "w", encoding="utf-8") as file:
+            file.write(text)
+        res = reader.read(message)
+        assert res.file_name == "translitText.txt"
+        assert res.sender == "studentochki.com"
+        assert res.subject == "pomogite skoro sessia"
+    def test_noSender(self, reader, tmp_path):
+        message = os.path.join(tmp_path, "withoutSender.txt")
+        text = "Subject: ляляляля"
+        with open(message, "w", encoding="utf-8") as file:
+            file.write(text)
+        res = reader.read(message)
+        assert res.file_name == "withoutSender.txt"
+        assert res.sender == ""
+        assert res.subject == "ляляляля"
+    def test_noSubject(self, reader, tmp_path):
+        message = os.path.join(tmp_path, "withoutSubject.txt")
+        text = "От кого: ychebka.ru"
+        with open(message, "w", encoding="utf-8") as file:
+            file.write(text)
+        res = reader.read(message)
+        assert res.file_name == "withoutSubject.txt"
+        assert res.sender == "ychebka.ru"
+        assert res.subject == ""
+    def test_noSenderNoSubject(self, reader, tmp_path):
+        message = os.path.join(tmp_path, "nothing.txt")
+        text = "ааа"
+        with open(message, "w", encoding="utf-8") as file:
+            file.write(text)
+        res = reader.read(message)
+        assert res.file_name == "nothing.txt"
+        assert res.sender == ""
+        assert res.subject == ""
 
 class TestFileManager:
     def test_move_file_creates_category_folder_and_moves_file(self, tmp_path, monkeypatch):
